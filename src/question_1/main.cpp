d5e9506e4414dbdf8cd51df67115c011944cf811
Milad Adarkani <143145275+MAdarkani@users.noreply.github.com>
2023-10-14 04:25:36 +0000

    Question 1

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,33 @@
+#include <iostream>
+#include "question1.h"
+
+using::std::cout; using::std::cin;
 
 int main()
-{
+{  
+    char option;
 
+    do
+    {
+        int widgets_sold;
+        int earned_points;
+
+        cout<<"Enter the number of widgets sold: \n";
+        cin>>widgets_sold;
+        if (widgets_sold>=0)
+        {
+            earned_points = get_earned_points(widgets_sold);
+            cout<<"Points earned: "<<earned_points<<"\n";
+        }
+        else
+        {
+            cout<<"You entered invalid number.\n";
+        }
+        
+        cout<<"Do you want to calculate another points? y/n \n";
+        cin>>option;
+
+    } while (option == 'Y' || option == 'y');
+    
     return 0;
 }
--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -4,3 +4,28 @@ bool test_config()
 {
     return true;
 }
+
+int get_earned_points(int sold)
+{
+    int points_earned = 0;
+
+    if (sold>=1 && sold<=5)
+    {
+        points_earned = 1*sold;
+    }
+    else if (sold>=6 && sold<=10)
+    {
+        points_earned = 5*sold;
+    }
+    else if (sold>=11 && sold<=15)
+    {
+        points_earned = 10*sold;
+    }
+    else if (sold>=16)
+    {
+        points_earned = 15*sold;
+    }
+    
+    return points_earned;
+    
+}
